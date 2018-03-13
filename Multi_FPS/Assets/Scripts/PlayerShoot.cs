a6cc90459d6db2110e1cfde83f454727f88c4ff4
Troels Jensen <jensen_troels@hotmail.com>
2018-03-13 11:26:45 +0100

    added kill count function

---
+++ Multi_FPS/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+
+    public Camera shootCam;
+    public float weaponDamage = 10f;
+    public float scaleChangeFactor = 0.1f;
+
+    private Vector3 scaleChange;
+    private Player selfPlayer;
+
+    private void Start()
+    {
+        scaleChange = new Vector3(scaleChangeFactor, scaleChangeFactor, scaleChangeFactor);
+        selfPlayer = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot()
+    {
+        RaycastHit targetHit;
+        if (Physics.Raycast(shootCam.transform.position, shootCam.transform.forward, out targetHit))
+        {
+            if (targetHit.transform.CompareTag("Player"))
+            {
+                CmdFire(targetHit.transform.name, weaponDamage, scaleChange);
+                selfPlayer.CmdChangeScale(scaleChange);
+            }
+        }
+    }
+
+    [Command]
+    private void CmdFire(string targetName, float damage, Vector3 scaleDamage)
+    {
+        Player targetPlayer = GameManager.GetPlayer(targetName);
+
+        targetPlayer.RpcTakeHealthDamage(damage, this.GetComponent<NetworkIdentity>());
+        targetPlayer.RpcChangeScale(-scaleDamage);
+    }
+}
+
+
+
