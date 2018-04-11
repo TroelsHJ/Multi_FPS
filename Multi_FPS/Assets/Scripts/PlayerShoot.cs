6942ae3fbe381ef4a9401e17747977b0fc9c5951
Troels Højmark Jensen <troe3894@student.eadania.dk>
2018-04-11 14:04:21 +0200

    added hit effects

--- Multi_FPS/Assets/Scripts/PlayerShoot.cs
+++ Multi_FPS/Assets/Scripts/PlayerShoot.cs
@@ -10,7 +10,8 @@ public class PlayerShoot : NetworkBehaviour
     public float weaponDamage = 10f;
     public float scaleChangeFactor = 0.1f;
     public ParticleSystem muzzleFlash;
-    public ParticleSystem smokeEffect;
+    public ParticleSystem shootSmokeEffect;
+    public GameObject hitEffect;
 
     private Vector3 scaleChange;
     private Player selfPlayer;
@@ -29,11 +30,45 @@ public class PlayerShoot : NetworkBehaviour
         }
     }
 
+    // Is called on the server when the player shoots
+    [Command]
+    void CmdOnShoot()
+    {
+        RpcDoShootEffect();
+    }
+
+    // Is called on all clients when we need to display shoot effect
+    [ClientRpc]
+    void RpcDoShootEffect()
+    {
+        muzzleFlash.Play();
+        shootSmokeEffect.Play();
+    }
+
+    [Command]
+    void CmdOnHit(Vector3 _pos, Vector3 _normal)
+    {
+        RpcDoImpactEffect(_pos, _normal);
+    }
+
+    [ClientRpc]
+    void RpcDoImpactEffect(Vector3 _pos, Vector3 _normal)
+    {
+        GameObject _tempHitEffect = (GameObject)Instantiate(hitEffect, _pos, Quaternion.LookRotation(_normal));
+        Destroy(_tempHitEffect, 1f);
+    }
+
     [Client]
     void Shoot()
     {
-        muzzleFlash.Play();
-        smokeEffect.Play();
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        // We are shooting, call the OnShoot method on the server.
+        CmdOnShoot();
+
         RaycastHit targetHit;
         if (Physics.Raycast(shootCam.transform.position, shootCam.transform.forward, out targetHit))
         {
@@ -42,6 +77,8 @@ public class PlayerShoot : NetworkBehaviour
                 CmdFire(targetHit.transform.name, weaponDamage, scaleChange);
                 selfPlayer.CmdChangeScale(scaleChange);
             }
+            // We hit something - call OnHit on server with data
+            CmdOnHit(targetHit.point, targetHit.normal);
         }
     }
 
